35d9b6f1e2eb1cac0f78dad9df22dd22bc67cfd2
Deer-Garden <willkinsongs.alais@gmail.com>
2021-03-02 19:08:36 +0900

    checkbox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 (function() {
   'use strict';
+
+  const vm = new Vue({
+    el: '#app',
+    data: {
+        newItem: '',
+        todos: [{
+          title: 'task 1',
+          isDone: false
+        }, {
+          title: 'task 2',
+          isDone: false
+        }, {
+          title: 'task 3',
+          isDone: true
+        }]
+    },
+    methods: {
+      addItem: function() {
+        const item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('消しますか？')) {
+          this.todos.splice(index, 1);
+        }
+      }
+    }
+  });
 })();
