ade238fb2234dc2272d4045f43d63b90cb480c5d
Deer-Garden <willkinsongs.alais@gmail.com>
2021-03-03 20:36:09 +0900

    purge

--- js/main.js
+++ js/main.js
@@ -5,17 +5,16 @@
     el: '#app',
     data: {
         newItem: '',
-        // todos: [{
-        //   title: 'task 1',
-        //   isDone: false
-        // }, {
-        //   title: 'task 2',
-        //   isDone: false
-        // }, {
-        //   title: 'task 3',
-        //   isDone: true
-        // }]
-        todos: []
+        todos: [{
+          title: 'task 1',
+          isDone: false
+        }, {
+          title: 'task 2',
+          isDone: false
+        }, {
+          title: 'task 3',
+          isDone: true
+        }]
     },
     methods: {
       addItem: function() {
@@ -29,6 +28,19 @@
       deleteItem: function(index) {
         if (confirm('消しますか？')) {
           this.todos.splice(index, 1);
+        }
+      },
+      purge: function() {
+        if (!confirm('達成したものを削除する？')) {
+          return;
+        }
+      },
+      computed: {
+        remaining: function() {
+          const items = this.todos.filter(function(todo) {
+            return !todo.isDone;
+          });
+          return items.length;
         }
       }
     }
