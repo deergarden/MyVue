ba0e429ee8574fce98fd8ff3aab8c984183e2661
Deer-Garden <willkinsongs.alais@gmail.com>
2021-03-05 16:00:10 +0900

    エラー解消

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@
   const vm = new Vue({
     el: '#app',
     data: {
-        newItem: '',
-        todos: [{
-          title: 'task 1',
-          isDone: false
-        }, {
-          title: 'task 2',
-          isDone: false
-        }, {
-          title: 'task 3',
-          isDone: true
-        }]
+      newItem: '',
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true
+      }]
     },
     methods: {
       addItem: function() {
@@ -29,20 +29,14 @@
         if (confirm('消しますか？')) {
           this.todos.splice(index, 1);
         }
-      },
-      purge: function() {
-        if (!confirm('達成したものを削除する？')) {
-          return;
-        }
-      },
-      computed: {
-        remaining: function() {
+      }
+    },
+    computed: {
+      remaining: function() {
           const items = this.todos.filter(function(todo) {
-            return !todo.isDone;
-            
-          });
-          return items.length;
-        }
+          return !todo.isDone;
+        });
+        return items.length;
       }
     }
   });
